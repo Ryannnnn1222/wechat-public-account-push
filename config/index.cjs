402b40453f8e184105f9984ab6dd9cd02cc9df3f
Ryannnnn1222 <127391985+Ryannnnn1222@users.noreply.github.com>
2025-01-27 20:17:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,38 +8,38 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxa895c6f67ca87d6a',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '1f3a52e3255e6ffc7a4af2e6054bb5fe',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '河北',
+  CITY: '邢台',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '帅飞哥',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oeSRN7AnuFJ0Rmh9CgC472aC2PXQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'SIXjLChHSDV_7QlSPquqNIPHMa47rOfPBdE_HXdYJj0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
+      // festivals: [
+      //   // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+      //   {
+      //     type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+      //   },
+      //   // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+      //   {
+      //     type: '生日', name: '李四', year: '1996', date: '09-31',
+      //   },
+      //   {
+      //     type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+      //   },
+      // ],
+      我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2022-09-08' },
@@ -55,9 +55,9 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '帅飞哥',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oeSRN7AnuFJ0Rmh9CgC472aC2PXQ',
     }
   ],
 
